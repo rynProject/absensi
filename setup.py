6646c5ddd812abf053479a8b787736f33ddf4ff5
rynproject <rmj634@gmail.com>
2020-07-25 20:40:56 +0700

    add sql file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='absensi',
+    version='0.0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='user',
+    author_email='',
+    description=''
+)
